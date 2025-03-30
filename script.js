feeb46d1aee2c71e4247cafd0053440c49b1fda6
Mrs Crypt <reokaro89@gmail.com>
2025-03-30 13:01:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Lampu minimalist
 const lampuContainer = document.getElementById('lampuContainer');
 for (let i = 0; i < 5; i++) {
   const lampu = document.createElement('div');
@@ -7,11 +6,10 @@ for (let i = 0; i < 5; i++) {
   lampuContainer.appendChild(lampu);
 }
 
-// Pelita icon
 const pelitaRow = document.getElementById('pelitaRow');
 for (let i = 0; i < 2; i++) {
   const pelita = document.createElement('img');
-  pelita.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Pelita_icon.svg/1024px-Pelita_icon.svg.png";
+  pelita.src = "https://upload.wikimedia.org/wikipedia/commons/9/9e/Pelita_icon.png";
   pelita.alt = "Pelita Raya";
   pelita.className = "pelita";
   pelitaRow.appendChild(pelita);
